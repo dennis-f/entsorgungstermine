19d2951e3b8bd2eb79b2bee8e6f39493db140d93
max <hello@maxbehr.de>
2017-10-31 10:44:04 +0100

    Remove check for console logs

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    'no-console': 'off',
     'no-unused-vars': 'off',
     // allow paren-less arrow functions
     'arrow-parens': 0,
