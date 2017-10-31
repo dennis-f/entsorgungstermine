65dfe124993d6bc31b17da05f9221b46d422be74
max <hello@maxbehr.de>
2017-10-31 10:44:46 +0100

    Add vuex store and action to query for street names

---
+++ src/store/store.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+	state: {
+		streets: []
+	},
+	actions: {
+		LOAD_CONFIG: function ({ commit }) {
+      let self = this;
+      let uri = '../static/data/potsdam/streetnames.json';
+      axios.get(uri)
+      .then(function (response) {
+        commit('SET_CONFIG', { list: response.data });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+  },
+  mutations: {
+    SET_CONFIG: (state, { list }) => {
+      state.streets = list;
+    }
+  },
+  getters: {
+    getStreets: state => {
+      return state.streets;
+    }
+  }
+})
+
+export default store
